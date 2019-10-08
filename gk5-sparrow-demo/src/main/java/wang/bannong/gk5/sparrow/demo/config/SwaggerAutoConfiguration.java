2e8a67d482255e2b60cf608e63c9b54f89effe4b
bn <bannongvipp@163.com>
2019-10-08 14:01:52 +0800

    🎯swagger配置

--- gk5-sparrow-demo/src/main/java/wang/bannong/gk5/sparrow/demo/config/SwaggerAutoConfiguration.java
+++ gk5-sparrow-demo/src/main/java/wang/bannong/gk5/sparrow/demo/config/SwaggerAutoConfiguration.java
@@ -1,4 +1,4 @@
-package wang.bannong.gk5.sparrow.config;
+package wang.bannong.gk5.sparrow.demo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -21,16 +21,14 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 import wang.bannong.gk5.sparrow.common.utils.JWTUtils;
 
 /**
- * RESTful 服务 API 管理框架 Swagger 配置初始化
+ * Created by bn. on 2019/10/8 1:55 PM
  */
 @Configuration
 @EnableSwagger2
 public class SwaggerAutoConfiguration {
-
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
-                .host("https://bn.zdautoservice.com")
                 .globalOperationParameters(getParameters())
                 .apiInfo(apiInfo())
                 .select()
