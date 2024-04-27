c70acaa0be55492a433ab9c71eca2f7a79112bcc
bn <bannongvipp@163.com>
2024-04-27 20:18:50 +0800

    验证HashMap不可以使用在多线程环境

---
+++ gk5-offer/gk5-offer-jdk/src/main/java/wang/bannong/gk5/offer/jdk/collect/HashMapInfiniteLoop.java
@@ -0,0 +1,32 @@
+package wang.bannong.gk5.offer.jdk.collect;
+
+import java.util.HashMap;
+
+/**
+ * @author <a href="mailto:bannongvipp@163.com">bn</a>
+ * @date 2024/4/22
+ */
+public class HashMapInfiniteLoop {
+    private static HashMap<Integer, String> map = new HashMap<>(2, 0.75f);
+
+    public static void main(String[] args) {
+        map.put(5, "C");
+
+        new Thread("Thread1") {
+            public void run() {
+                map.put(7, "B");
+                System.out.println(map);
+            }
+
+            ;
+        }.start();
+        new Thread("Thread2") {
+            public void run() {
+                map.put(3, "A");
+                System.out.println(map);
+            }
+
+            ;
+        }.start();
+    }
+}
